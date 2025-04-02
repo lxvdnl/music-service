0b07f29234459ff48a89f510e41d01ebea49bace
Lxvdnl <alexseev_dnl@mail.ru>
2025-04-02 11:36:04 +0300

    add validation for UserDto

--- user/src/main/java/com/lxvdnl/user/web/UserController.java
+++ user/src/main/java/com/lxvdnl/user/web/UserController.java
@@ -2,14 +2,17 @@ package com.lxvdnl.user.web;
 
 import com.lxvdnl.user.model.User;
 import com.lxvdnl.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @Slf4j
+@Validated
 @RestController
 @RequestMapping("/api/v1/users")
 @AllArgsConstructor
@@ -19,7 +22,7 @@ public class UserController {
     private final UserMapper userMapper;
 
     @PostMapping
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
         log.info("Creating user: name={}, username={}", userDto.getName(), userDto.getUsername());
         User newUser = userService.createUser(userMapper.toEntity(userDto));
         log.info("Successfully created user: id={}, name={}, username={}", newUser.getId(), newUser.getName(), newUser.getUsername());
@@ -35,7 +38,7 @@ public class UserController {
     }
 
     @PutMapping("/{userId}")
-    public ResponseEntity<UserDto> updateUser(@PathVariable("userId") UUID userId, @RequestBody UserDto userDto) {
+    public ResponseEntity<UserDto> updateUser(@PathVariable("userId") UUID userId, @Valid @RequestBody UserDto userDto) {
         User user = userMapper.toEntity(userDto);
         user.setId(userId);
         log.info("Updating user: id={}, name={}, username={}", user.getId(), user.getName(), user.getUsername());
@@ -53,5 +56,3 @@ public class UserController {
     }
 
 }
-
-// todo: validation
--- user/src/main/java/com/lxvdnl/user/web/UserDto.java
+++ user/src/main/java/com/lxvdnl/user/web/UserDto.java
@@ -1,5 +1,9 @@
 package com.lxvdnl.user.web;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,8 +15,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserDto {
 
+    @NotBlank(message = "Name can't be empty")
+    @Size(min = 1, max = 50, message = "Name size must be between 1 and 50")
     private String name;
+
+    @NotBlank(message = "Username can't be empty")
+    @Size(min = 1, max = 256, message = "Username size must be between 1 and 256")
+    @Email(message = "Incorrect email")
     private String username;
+
+    @NotBlank(message = "Password can't be empty")
+    @Size(min = 8, message = "Password must have at least 8 characters")
+    @Pattern(regexp = ".*\\d.*", message = "Password must have at least 1 number")
     private String password;
 
 }
