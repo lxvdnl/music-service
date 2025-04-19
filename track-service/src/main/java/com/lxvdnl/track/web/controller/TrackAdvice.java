157793b0dfa3e9666569390300471640ec36c72b
Lxvdnl <alexseev_dnl@mail.ru>
2025-04-19 21:29:44 +0300

    add exceptionHandler for UserNotFoundException

--- track-service/src/main/java/com/lxvdnl/track/web/controller/TrackAdvice.java
+++ track-service/src/main/java/com/lxvdnl/track/web/controller/TrackAdvice.java
@@ -2,6 +2,7 @@ package com.lxvdnl.track.web.controller;
 
 import com.lxvdnl.track.exception.TrackNotFoundException;
 import com.lxvdnl.track.exception.UploadException;
+import com.lxvdnl.track.exception.UserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,12 @@ public class TrackAdvice {
 
     @ExceptionHandler(TrackNotFoundException.class)
     public ResponseEntity<String> handleTrackNotFoundException(TrackNotFoundException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(UserNotFoundException e) {
         log.error(e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
